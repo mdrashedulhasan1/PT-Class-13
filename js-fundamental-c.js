6ad1af037891cec20a1e5daa343ebc6beb337316
Rashedul Hasan <Rashedul Hasan>
2022-03-14 16:48:18 +0600

    Fundamental-Concept

---
+++ js-fundamental-c.js
@@ -0,0 +1,78 @@
+//Array Index, Set by Index, IndexOf
+// var friendsAge = [12, 23, 34, 45, 56];
+// friendsAge[0] = 21;
+// var position = friendsAge.indexOf(21);
+// console.log(position);
+// var asif = friendsAge[0];
+// console.log(friendsAge);
+
+
+//push,pop,unshift,shift
+// var friendList = ['sarowal', 'rakib']
+// friendList.push('tapu');
+// friendList.pop();
+// friendList.unshift('rashedul');
+// friendList.shift();
+// console.log(friendList);
+
+//While
+// var num = 1;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+//For
+// for(var i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+//Switch
+// var num = 2;
+// switch(num){
+//     case 1:
+//         console.log("One");
+//         break;
+//     case 2:
+//         console.log("Two");
+//         break;
+//     case 3:
+//         console.log("Three");
+//         break;
+//     case 4:
+//         console.log("Four");
+//         break;
+//     case 5:
+//         console.log("Five");
+//         break;
+//     default:
+//         console.log("Number Not Found!");
+// }
+
+
+// function info(){
+//     console.log("Name: Tapu");
+//     console.log("Address: Tangail");
+// }
+// info();
+// info();
+// info();
+
+
+// function add(num1, num2){
+//     return num1+num2;
+// }
+// var result = add(2, 5);
+// console.log(result);
+
+
+//Object
+// var person = {id:01, name:"Tapu", job:"Web Development"};
+
+// console.log(person.id);
+// console.log(person['job']);
+// console.log(person);
+
+// console.log(person.name = "Rashedul");
+// console.log(person['job'] = "MERN Stack");
+// console.log(person);
